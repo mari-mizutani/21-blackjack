58a5180b702e8ac879e0012088505d9f4e0af52a
mari-mizutani <82095084+mari-mizutani@users.noreply.github.com>
2021-04-30 00:36:11 +0200

    2nd commit

--- script.js
+++ script.js
@@ -8,16 +8,25 @@ let deck = [1,2,3,4,5,6,7,8,9,10,10,10,10,
 let playerHand ;
 let dealerHand ;
 
+//randomly choose
+function drawRandomCard(deck){
+    let randomChoice = Math.floor(deck.length * Math.random());
+    return deck[randomChoice];
+}
+
+//calculate the cards
+function getHandValue(hand){
+    let sum = 0;
+    for(let i=0; i<hand.length; i++){
+    sum += hand[i];
+    } 
+    return sum;   
+}; 
+
 
 //START -- click and show first 2 cards of player
 document.getElementById("start").addEventListener("click", function() {  
-
-    //randomly choose
-    function drawRandomCard(deck){
-        let randomChoice = Math.floor(deck.length * Math.random());
-        return deck[randomChoice];
-    }
-
+  
     //first 2cards for each
     function startGame(){
         playerHand = [drawRandomCard(deck),drawRandomCard(deck)];
@@ -25,41 +34,65 @@ document.getElementById("start").addEventListener("click", function() {
     }
     startGame(); 
 
-
     //show the cards
     document.getElementById("player-hand").innerHTML = playerHand; 
-    document.getElementById("dealer-hand").innerHTML = dealerHand; 
-
-    //calculate the cards
-    function getHandValue(hand){
-        let sum = 0;
-        for(let i=0; i<hand.length; i++){
-        sum += hand[i];
-        } 
-        return sum;   
-       }; 
+    document.getElementById("dealer-hand").innerHTML = "?"; 
 
 
     //show the total values
     document.getElementById("player-hand-value").innerHTML = getHandValue(playerHand); 
-    document.getElementById("dealer-hand-value").innerHTML = getHandValue(dealerHand); 
 });
 
 
 //HIT --- click and give 1 card
 document.getElementById("hit").addEventListener("click", function() {  
+
     function hit(){
-        playerHand.push(drawRandomCard(deck));
-        if(getHandValue(playerHand) > 21){
-          document.getElementById("game-status").innerHTML= "BUST!";
-        }
-});  
-
-    document.getElementById("player-hand").innerHTML = playerHand;
-    document.getElementById("player-hand-value").innerHTML = getHandValue(playerHand) ;
-    document.getElementById("dealer-hand").innerHTML = dealerHand;
-    document.getElementById("dealer-hand-value").innerHTML = getHandValue(dealerHand) ;
+       playerHand.push(drawRandomCard(deck));
+       dealerHand.push(drawRandomCard(deck));
+    } 
+    hit()
+    
+    //show the cards
+    document.getElementById("player-hand").innerHTML = playerHand; 
 
 
+    //if the total become over 21, says 'BUST'
+    if(getHandValue(playerHand)>21){
+        document.getElementById("player-hand-value").innerHTML = getHandValue(playerHand)  + "  BUST!"; 
+        //show the total values 
+        document.getElementById("dealer-hand-value").innerHTML = getHandValue(dealerHand); 
+        document.getElementById("dealer-hand").innerHTML = dealerHand; 
+    }else{
+        document.getElementById("player-hand-value").innerHTML = getHandValue(playerHand); 
+    }
+    
+});
 
+
+//STAND
+document.getElementById("stand").addEventListener("click", function() {  
+    //if the total become over 21, says 'BUST'
+    if(getHandValue(playerHand)<21 && 21 - getHandValue(playerHand) < 21 - getHandValue(dealerHand)){
+        document.getElementById("player-hand-value").innerHTML = getHandValue(playerHand)  + "  WIN!"; 
+        document.getElementById("dealer-hand").innerHTML = dealerHand; 
+        document.getElementById("dealer-hand-value").innerHTML = getHandValue(dealerHand);
+    }
+    else if(getHandValue(playerHand)<21 && getHandValue(dealerHand>21)){
+        document.getElementById("player-hand-value").innerHTML = getHandValue(playerHand)  + "  WIN!"; 
+        document.getElementById("dealer-hand").innerHTML = dealerHand; 
+        document.getElementById("dealer-hand-value").innerHTML = getHandValue(dealerHand);
+    }
+    else if(getHandValue(playerHand)>21 && getHandValue(dealerHand<21)){
+        document.getElementById("player-hand-value").innerHTML = getHandValue(playerHand)  + "  LOSE!"; 
+        document.getElementById("dealer-hand").innerHTML = dealerHand; 
+        document.getElementById("dealer-hand-value").innerHTML = getHandValue(dealerHand);
+    }
+    else if(21 - getHandValue(playerHand) == 21 - getHandValue(dealerHand)){
+        document.getElementById("player-hand-value").innerHTML = getHandValue(playerHand)  + "  TIE!"; 
+        document.getElementById("dealer-hand").innerHTML = dealerHand; 
+        document.getElementById("dealer-hand-value").innerHTML = getHandValue(dealerHand);
+    }
+
+});
 
